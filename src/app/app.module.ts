7d157a79910b53c853fc42d42b0eff6dffa79766
core.editor <an.munozq@duocuc.cl>
2023-08-27 16:54:22 -0400

    Estructura login con Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => {
+      const auth = getAuth();
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      connectFirestoreEmulator(firestore, 'http://localhost', 8080);
+      return firestore;
+    }),
+  ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'login-firebase-angular-a8f91',
+    appId: '1:854769019182:web:65814aa89ee87d3188b2bf',
+    storageBucket: 'login-firebase-angular-a8f91.appspot.com',
+    apiKey: 'AIzaSyBeQ-fRLG-NlG-u6_QEiQnlo-eH4G1-v2c',
+    authDomain: 'login-firebase-angular-a8f91.firebaseapp.com',
+    messagingSenderId: '854769019182',
+  },
   apiUrl: '',
   production: true,
 };
